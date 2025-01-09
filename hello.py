ac748aa780e8ff11236a59ddf99f15b661b38790
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2025-01-09 23:36:40 +0100

    Added pyright imports

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import sys
+
+
 def main():
     print("Hello from numal!")
     if sys.version_info[0] < 3:
-     raise Exception("Must be using Python 3")
+        raise Exception("Must be using Python 3")
     print(sys.version_info)
 
 
--- introduction.py
+++ introduction.py
@@ -1,17 +1,17 @@
+import numpy as np
 
 n = [2, 4, 5, 6, 7]
+
 
 def mean(arr):
     xsum = 0
     x2sum = 0
-    n = len(arr) 
+    n = len(arr)
     for xi in range(0, n + 1):
         xsum = xsum + xi
         x2sum = x2sum + (xi * xi)
-    xbar = xsum / n 
+    xbar = xsum / n
     return xbar
 
 
-
 print(mean(n))
-
